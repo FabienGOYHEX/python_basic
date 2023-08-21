1634b755e872c24d05f2a33a285b6f2515e1a10f
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-21 16:11:49 +0200

    python types

--- index.py
+++ index.py
@@ -1,4 +1,28 @@
 
 # commentaire
-variable_name = 'hello world depuis la variable'
-print(variable_name)
+car_model = 'Renault Twingo'
+nbr_of_wheels = '4'
+print(f"Le modèle de voiture {car_model} dispose de {nbr_of_wheels} roues.")
+# int
+a = 1
+# float
+b = -2, 4
+# str
+c = 'hello world'
+# bool
+d = True
+# list // les valeurs d'une liste sont modifiables
+e = [1, 'hello', False]
+# tuple // les valeurs dun tuple ne sont plus modifiables une fois assignés (imutable)
+f = (2, 'world', True)
+# set //similaire à la liste, les valeurs dun set sont modifiables, IL NE PEUT Y AVOIR DE DOUBLONS
+g = {3, 'Bonjour', 2.2}
+# dic //similaire au set (valeurs modifiables pas de doublons)avec clés : valeurs
+h = {
+    'fr_bonjour ': 'Bonjour',
+    'en_bonjour ': 'hello',
+    'es_bonjour ': 'Hola'
+}
+# none
+i = None
+print(type(a))
