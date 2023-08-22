ada63a0189961c3b3e7f698c3184e6339d7584bb
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-22 12:11:29 +0200

    LOOP

---
+++ loop.py
@@ -0,0 +1,12 @@
+
+#for
+my_list = [3,44,22,10,-5]
+counter = 0
+for i in my_list :
+    counter += i
+print( counter)
+
+#while
+age = 0 
+while int(age) < 18 :
+    age = input('Quel est votre age ? ')
