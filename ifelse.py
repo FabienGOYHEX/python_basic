b2cd6123d575c852e3a57a23a0ed5738dcfc2805
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-22 10:57:09 +0200

    opérateurs logiques & match

--- ifelse.py
+++ ifelse.py
@@ -8,3 +8,26 @@ elif number == 500 :
     print('yesssss')
 else :
     print('nope')
+
+    # Opérateurs logique :  and, or, not
+
+is_adult =  True
+is_rich = True
+
+if is_adult and is_rich :
+    print('Bienvenue à bord de space X')
+else : print('Nope')
+
+#Match
+error = 404
+#
+match error:
+    case 500:
+        print('erreur serveur')
+    case 400:
+        print('Mauvaise requête')
+    case 404:
+        print('ressource nontrouvée')
+    case _ : 
+        print('error iconnue ')
+        
