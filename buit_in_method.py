321f2d0cb35375d64469decafe559d468843349e
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-22 21:54:24 +0200

    some built in method

---
+++ buit_in_method.py
@@ -0,0 +1,27 @@
+#abs
+print(abs(-5))
+# => 5
+
+#min
+print(min(1, 55, 66, 22, 13, 150, 0.2))
+# => 0.2
+
+#max
+print(max(1, 55, 66, 22, 13, 150, 0.2))
+# => 150
+
+#pow 
+print((pow(5,2)))
+# => 25 // 5^2 = 25
+
+#round 
+print(round(5.8))
+# => 6 
+
+#bin
+print(bin(5))
+# => 0b101 return la représentation du chiffre 5 en code binaire 
+
+#hex
+print(hex(16))
+# => 0x10 return la représentation du chiffre 16 en hexadecimal
