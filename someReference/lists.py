ea2710f21f79094c130b1ee6e5ab93b3ca9f93d7
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-25 16:48:07 +0200

    remove methods

--- someReference/lists.py
+++ someReference/lists.py
@@ -9,10 +9,14 @@ a = [1, 2, 'hello', True, 5, ['a', 2]]
 variable1, variable2, varaible3, *listedureste = a
 # => 1 2 hello [True, 5, ['a', 2]]
 
-
+# INSERT
 # append insèrera toujours la valeur ne dernier dans la liste
 a.append('InsertedValueWithHappendMethod')
 a.insert(0, -1)  # arg1 = index arg2= Valeur à insérer
-
 a.extend(['chien', 'chat'])
+
+# REMOVE
+a.remove('chat')  # on passe la valeur à suprimer
+a.pop(0)  # on passe l'index de la valeur à suprimer
+a.clear()  # on suprime toutes les valeurs
 print(a)
