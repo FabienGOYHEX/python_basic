6774be863adc8712429bd90acd10e4efaa54dee9
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-25 17:46:28 +0200

    browse a dictionnary

--- someReference/dictionary.py
+++ someReference/dictionary.py
@@ -8,10 +8,35 @@ my_dictionnary = {
     }
 }
 my_new_dictinnary = dict(deco='vintage', tapis="blue", tYpe="house")
+# Retourne une erreur si la key n'hexiste pas !!
 
 print(my_dictionnary['adress']['city'])
 print(my_new_dictinnary)
 
 print(my_dictionnary.get('age'))
 #fucking_dictionnary = my_dictinnary.copy()
-# Retourne une erreur si la key n'hexiste pas !!
+
+
+# PARCOURIR LES VALEURS
+
+# for i in my_dictionnary.values():
+# print(i)
+# => .keys() => age sexe firstName LastName adress
+# => .value() => 18 M Jean BON {'city': 'Paris'}
+# => .items() => ('age', 18) ('sexe', 'M') ('firstName', 'Jean') ('LastName', 'BON') ('adress', {'city': 'Paris'})
+
+# DECONSTRUCTION DE LA PAIRE CLE VALEUR D'UN DICTIONNAIRE
+
+for key, value in my_dictionnary.items():
+    print(key)
+    print(value)
+# => age
+# 18
+# sexe
+# M
+# firstName
+# Jean
+# LastName
+# BON
+# adress
+#{'city': 'Paris'}
