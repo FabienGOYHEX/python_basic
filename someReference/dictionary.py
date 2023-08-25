1fcd9eb2c3421b4c6a771265fa8a03cf1452cfba
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-25 17:20:24 +0200

    Dictionary

---
+++ someReference/dictionary.py
@@ -0,0 +1,12 @@
+my_dictionnary = {
+    'age': 18,
+    'sexe': 'M',
+    'firstName': 'Jean',
+    'LastName': 'BON',
+    'adress': {
+        'city': 'Paris'
+    }
+}
+print(my_dictionnary['adress']['city'])
+
+# Retourne une erreur si la key n'hexiste pas !!
