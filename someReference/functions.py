e402cf2ed532408a654f521b687a84a0f0434268
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-28 20:46:45 +0200

    first function example

--- someReference/functions.py
+++ someReference/functions.py
@@ -1,1 +1,6 @@
 # //////////////////////////////FUNCTIONS/////////////////////////////
+def my_funtion(a, b=2):  # valeur par défaut  de b =2 di non spécifié
+    return a + b
+
+
+print(my_funtion(1, 6))
