d32b68ecfc3f3bcfd4dfbb4b9e4273ba92496350
fabiengoyhex <fabien.goyhex@gmail.com>
2023-08-29 21:06:07 +0200

    global & nonlocal

--- someReference/functions.py
+++ someReference/functions.py
@@ -5,3 +5,17 @@ def my_funtion(a, b=2):  # valeur par défaut  de b =2 di non spécifié
 
 print(my_funtion(1, 6))
 # ///////// scope des fonctions //////////////
+# read the python doc
+
+a = 1
+
+# Pour pouvoir modifier une variable globale dans une fonction, il faut obligatoirement utiliser le mot clé global qui permet d'assurer à Python que nous modifions intentionnellement une variable globale :
+
+
+def fonction():
+    global a
+    a += 1
+
+
+fonction()
+# L'instruction nonlocal permet d'indiquer qu'une variable doit être cherchée dans la portée de la fonction parente.
